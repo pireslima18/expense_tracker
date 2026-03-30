d635bfac3ff0884b7688b16d5e8fca8de98563aa
Arthur Lima <arthurlima@evocaching.corp.medgrupo.net>
2026-03-30 12:04:33 -0300

    main: Subindo modal e campos de inputs

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:expense_racker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_racker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_racker/models/expense.dart';
 
@@ -29,9 +30,24 @@ class _ExpansesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay () {
+    showModalBottomSheet(context: context, builder: (ctx) => 
+      const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expenses App"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add)
+          )
+        ],
+      ),
       body: Column(
         children: [
           const Text("The chart"),
