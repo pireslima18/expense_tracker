f5c0387cd557adf4df445479278172cf729425e9
Arthur Lima <arthurlima@N01-1662-052596.corp.medgrupo.net>
2026-04-06 18:04:44 -0300

    master: Subindo configuração de theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,38 @@
 import 'package:expense_racker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorSchema = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+      useMaterial3: true,
+      colorScheme: kColorSchema,
+      appBarTheme:  const AppBarTheme().copyWith(
+        backgroundColor: kColorSchema.onPrimaryContainer,
+        foregroundColor: kColorSchema.primaryContainer,
+        centerTitle: false
+      ),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorSchema.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        )
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorSchema.primaryContainer
+        ),
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: kColorSchema.onSecondaryContainer,
+          fontSize: 14,
+        )
+      )
+    ),
+    home: const Expenses(),
   ));
 }
